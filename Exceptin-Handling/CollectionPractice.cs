62f2012a70ff23c84d20d19c64691374da4dce0f
yashweybee <yashparmar@weybee.in>
2023-10-04 12:53:07 +0530

    Collections completed

---
+++ Exceptin-Handling/CollectionPractice.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptin_Handling
+{
+    class CollectionPractice
+    {
+        static void Main(string[] args)
+        {
+            //list
+            var list1 = new List<string>();
+            list1.Add("asdf");
+            list1.Add("qwer");
+            list1.Add("zxc");
+            list1.Add("dfgh");
+
+            List<int> intList = new List<int>();
+            intList.Add(12);
+            intList.Add(20);
+            //intList.Remove(12);
+            //intList.Reverse();
+
+            var list2 = new List<int>() { 12, 45, 23, 456, 23 };
+
+
+
+
+            //HashSet
+            //HashSet<string> names = new HashSet<string>();
+            var names = new HashSet<string>();
+            names.Add("aaa");
+            names.Add("bbb");
+            names.Add("aaa");//not added
+
+            //Sortedset
+            var firstNames = new SortedSet<string>();
+            firstNames.Add("opopop");
+            firstNames.Add("xzcbf");
+            firstNames.Add("opopop");
+            firstNames.Add("asdf");
+            //Console.WriteLine(firstNames.Count());
+
+
+            //stack
+            var myStack = new Stack<int>();
+            myStack.Push(10);
+            myStack.Push(20);
+            myStack.Push(40);
+            myStack.Push(30);
+            //Console.WriteLine(myStack.Pop());
+            //Console.WriteLine(myStack.Peek());
+
+            //Queue
+            Queue<string> myQueue = new Queue<string>();
+            myQueue.Enqueue("aaaa");
+            myQueue.Enqueue("hjhjh");
+            myQueue.Enqueue("ioioio");
+
+            myQueue.Dequeue();
+
+            //LinkedList
+            var myLl = new LinkedList<string>();
+            myLl.AddLast("aaa");
+            myLl.AddLast("bbb");
+            myLl.AddFirst("ccc");
+
+            //dictionary
+            var myDec = new Dictionary<int, string>();
+            myDec.Add(1, "aa");
+            myDec.Add(2, "b");
+            myDec.Add(3, "c");
+            myDec.Add(4, "b");
+            //Console.WriteLine(myDec.Keys.Count());
+
+
+
+            //SortedDictionary
+            var sortedDec = new SortedDictionary<int, string>();
+            sortedDec.Add(1, "aa");
+            sortedDec.Add(3, "bb");
+            sortedDec.Add(4, "cc");
+            sortedDec.Add(2, "cc");
+
+
+            //sortedList
+            var mySortedList = new SortedList<int, string>();
+            mySortedList.Add(1, "aa");
+            mySortedList.Add(2, "kk");
+            mySortedList.Add(4, "aa");
+            mySortedList.Add(3, "aa");
+
+            foreach (KeyValuePair<int, string> kv in mySortedList)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+
+
+            //foreach (var i in myLl)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            Console.Read();
+        }
+    }
+}
--- Exceptin-Handling/Program.cs
+++ Exceptin-Handling/Program.cs
@@ -6,10 +6,67 @@ using System.Threading.Tasks;
 
 namespace Exceptin_Handling
 {
+    class ExceptHandle : Exception
+    {
+        public ExceptHandle(string Message) : base(Message) { }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            //try
+            //{
+            //    int a = 10;
+            //    int b = 0;
+            //    int c = a / b;
+            //    Console.WriteLine(c);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finally block");
+            //}
+            //Console.WriteLine("Rest Of the code");
+
+
+            //int age = 8;
+            //try
+            //{
+            //    if (age < 18) {
+            //        throw new ExceptHandle("Age > 18 is applicable");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("You can apply for license");
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //unchecked
+            //{
+            //int val = int.MaxValue;
+            //Console.WriteLine(val +2);
+            //}
+
+
+            try
+            {
+                int[] arr = new int[5];
+                arr[10] = 10;
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine(e.Data);
+            }
+            Console.WriteLine("Other code");
+
+            Console.Read();
         }
     }
 }
